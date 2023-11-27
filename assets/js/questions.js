9e6b39173e31b0569e9bb65ef560ee984296d7fc
yvonne <yvonnewong1107@gmail.com>
2023-11-27 20:05:03 +0000

    questions complete

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,15 +1,53 @@
+// https://www.interviewbit.com/javascript-mcq/
 var questions = [
     {
-        question: "What is 4 + 8",
-        answers: ["11",
-            "12",
-            "48",
-            "4848484"],
+        question: "JavaScript file has an extension of:",
+        answers: [
+            ".js",
+            ".javascript",
+            ".java",
+            ".jscript"
+        ],
+            correctAnswerIndex: 0
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        answers: [
+            "var",
+            "let",
+            "both of the above",
+            "none of the above"
+        ],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        answers: [
+            "getElementbyId()",
+            "getElementsByClassName()",
+            "both of the above",
+            "none of the above"
+        ],
+            correctAnswerIndex: 2
+    },
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        answers: [
+            "in",
+            "is in",
+            "exists",
+            "lies"
+        ],
+            correctAnswerIndex: 0
+    },
+    {
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        answers: [
+            "parse()",
+            "stringify()",
+            "convert()",
+            "none of the above"
+        ],
             correctAnswerIndex: 1
     },
-    {},
-    {},
-    {},
-    {},
-    {},
 ]
