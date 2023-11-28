84dc04e5a434177ff5e93dd2c83c40341dbce52a
yvonne <yvonnewong1107@gmail.com>
2023-11-28 00:07:58 +0000

    Logic for displaying question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,13 @@
 // start screen
 var startScreen = document.querySelector("#start-screen");
+var startButton = document.querySelector("#start");
 
 // questions 
-var questions = document.querySelector("#questions");
+var questionSection = document.querySelector("#questions");
+var questionTitleElement = document.querySelector("#question-title");
+var choicesElement = document.querySelector("#choices");
+//current question index
+var questionNumber = 0;
 
 // timer
 var initTime = 60; 
@@ -14,12 +19,39 @@ function startQuiz() {
     // hide start screen
     startScreen.classList.add("hide");
     // and show the questions
-    questions.classList.remove("hide");
+    questionSection.classList.remove("hide");
     // and show the timer which is currently hidden
     timer.classList.remove("hide");
     
+    // display questions 
+    displayQuestion();
 
 }
 
+// function to display question
+function displayQuestion() {
+    var question = questions[questionNumber];
+
+    // updates question 
+    questionTitleElement.textContent = question.question;
+
+    // for each answer, let's create a button
+    for (var i = 0; i < question.answers.length; i++) {
+        var choicesButton = document.createElement("button");
+
+        // setting attributes
+        choicesButton.textContent = question.answers[i];
+        choicesButton.setAttribute("data-index", i);
+        choicesButton.setAttribute("type", "submit");
+
+        // checking the clicked choice
+        choicesButton.addEventListener("click", function (event) {
+            var clickedChoice = parseInt(event.target.getAttribute("data-index"));
+            checkAnswer(clickedChoice);
+        });
+
+        choicesElement.appendChild(choicesButton);
+    }
+}
 // click event on start button
-document.querySelector("#start").addEventListener("click", startQuiz);
+startButton.addEventListener("click", startQuiz);
